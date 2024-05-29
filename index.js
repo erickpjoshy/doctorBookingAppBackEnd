fed0a081d3852373c589638d8534a5d0fda3f2f5
erickpjoshy <erickpjoshy@gmail.com>
2024-05-29 12:33:44 +0530

    5  commit

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ import dotenv from 'dotenv';
 import ejs from 'ejs';
 dotenv.config();
 const app = express();
+
+// CORS middleware setup
+const allowedOrigins = ['http://localhost:5174', 'http://127.0.0.1:5174'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 //malewares
 app.use(express.json());
-app.use(cors());
+
 app.use(routes);
 app.use(express.static('uploads'));
 app.set('view engine', 'ejs');
