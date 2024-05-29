d57f38ccb08066671e2ae78ed2d9d6cc8ec8f54e
erickpjoshy <erickpjoshy@gmail.com>
2024-05-29 12:11:33 +0530

    second  commit

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose
   .connect(
-    'mongodb+srv://erickpjoshy:lYnrLTMeBWAdlUwL@mebdoctortechdoctorbook.sx8coua.mongodb.net/?retryWrites=true&w=majority&appName=MebDoctorTechDoctorBookingApp'
+    'mongodb+srv://erickpjoshy:<password>@mebdoctortechdoctorbook.sx8coua.mongodb.net/?retryWrites=true&w=majority&appName=MebDoctorTechDoctorBookingApp'
   )
   .then(() => {
     console.log('DB Connected');
